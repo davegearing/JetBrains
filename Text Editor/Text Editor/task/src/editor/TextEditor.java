acad51b6d54cb399761bd713959b99e8f68185b4
DaveGearing <51530523+davegearing@users.noreply.github.com>
2020-07-10 18:58:50 +0100

    20200710
    had to nick file reader/writer as my scanner read and write failed jetBrains tests yet worked in real life

--- Text Editor/Text Editor/task/src/editor/TextEditor.java
+++ Text Editor/Text Editor/task/src/editor/TextEditor.java
@@ -1,19 +1,107 @@
 package editor;
 
 import javax.swing.*;
+import java.awt.*;
+import java.io.*;
+import java.nio.file.Files;
+import java.util.Scanner;
 
 public class TextEditor extends JFrame {
 
-    private JTextArea textArea;
+    final private JTextArea textArea;
+    final private JTextField filenameField;
+    final private JButton saveButton;
+    final private JButton loadButton;
+    final private JScrollPane scrollPane;
+    private String origfile = "";
 
     public TextEditor() {
-        super("The first stage");
+        super("Text Editor");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setSize(300, 300);
+        setSize(350, 350);
+
         textArea = new JTextArea();
         textArea.setName("TextArea");
-        add(textArea);
+        scrollPane = new JScrollPane(textArea);
+        scrollPane.setName("ScrollPane");
+        scrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
+        scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+        forceSize(scrollPane, 250, 250);
+        filenameField = new JTextField();
+        filenameField.setName("FilenameField");
+        forceSize(filenameField, 100, 20);
+        saveButton = new JButton();
+        saveButton.setName("SaveButton");
+        saveButton.setText("Save");
+        saveButton.addActionListener(actionEvent -> {
+            //Save code
+            File file = new File(filenameField.getText());
+            try (FileWriter fileWriter = new FileWriter(file)) {
+                fileWriter.write(textArea.getText());
+                fileWriter.flush();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            //String filename = filenameField.getText();
+            //if (!filename.equals("")) {
+            //    String newFile = textArea.getText();
+            //    try (FileWriter writer = new FileWriter(filename)) {
+            //        writer.write(newFile);
+            //    } catch (Exception e) {
+            //        e.printStackTrace();
+            //   }
+            //}
+        });
+        forceSize(saveButton, 80, 20);
+        loadButton = new JButton();
+        loadButton.setName("LoadButton");
+        loadButton.setText("Load");
+        loadButton.addActionListener(actionEvent -> {
+            //load code
+            File file = new File(filenameField.getText());
+
+            try {
+                if (file.createNewFile()) {
+                    textArea.setText("");
+                } else {
+                    textArea.setText(new String(Files.readAllBytes(file.toPath())));
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            //textArea.setText("");
+            //String filename = filenameField.getText();
+            //if (filename.equals("")) {
+            //    File file = new File(filename);
+            //    if (file.isFile()) {
+            //        try (Scanner scanner = new Scanner(file)) {
+            //            while (scanner.hasNextLine()) {
+            //                if (!textArea.getText().equals("")) {
+            //                    textArea.append("\n");
+            //                }
+            //                textArea.append(scanner.nextLine());
+            //            }
+            //        } catch (Exception e) {
+            //            e.printStackTrace();
+            //        }
+            //    }
+            //}
+            //origfile = textArea.getText();
+        });
+        forceSize(loadButton, 80, 20);
+
+        add(filenameField);
+        add(saveButton);
+        add(loadButton);
+        add(scrollPane);
         setVisible(true);
-        setLayout(null);
+        setLayout(new FlowLayout());
+    }
+
+    public static void forceSize(JComponent component, int width, int height) {
+        Dimension d = new Dimension(width, height);
+        component.setMinimumSize(d);/*from  www.javas.com*/
+        component.setMaximumSize(d);
+        component.setPreferredSize(d);
     }
 }
